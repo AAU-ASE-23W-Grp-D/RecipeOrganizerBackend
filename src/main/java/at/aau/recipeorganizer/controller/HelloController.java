fe0fcf6104d0d14af1d62ead88d5f925c9c4b919
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-03 21:30:34 +0100

    Move HelloController to package controller

--- src/main/java/at/aau/recipeorganizer/controller/HelloController.java
+++ src/main/java/at/aau/recipeorganizer/controller/HelloController.java
@@ -1,4 +1,4 @@
-package at.aau.recipeorganizer;
+package at.aau.recipeorganizer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
