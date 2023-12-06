518cebb16433432030af2ab126d21fca9ac2ffb4
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-06 22:11:09 +0100

    Configure CORS globally instead at controller level

--- src/main/java/at/aau/recipeorganizer/RecipeOrganizerApplication.java
+++ src/main/java/at/aau/recipeorganizer/RecipeOrganizerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -12,6 +14,19 @@ import java.util.Arrays;
 public class RecipeOrganizerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeOrganizerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT")
+                        .maxAge(3600);
+            }
+        };
     }
 
     @Bean
