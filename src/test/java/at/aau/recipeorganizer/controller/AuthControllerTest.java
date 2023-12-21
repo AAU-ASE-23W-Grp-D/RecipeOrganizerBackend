54b59d2340f7597162dd354406a295ff70443e0b
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-21 09:07:11 +0100

    Added mockMvc test for testing signin

--- src/test/java/at/aau/recipeorganizer/controller/AuthControllerTest.java
+++ src/test/java/at/aau/recipeorganizer/controller/AuthControllerTest.java
@@ -1,7 +1,10 @@
 package at.aau.recipeorganizer.controller;
 
 import at.aau.recipeorganizer.configuration.jwt.JwtUtils;
+import at.aau.recipeorganizer.data.LoginRequest;
+import at.aau.recipeorganizer.data.Role;
 import at.aau.recipeorganizer.data.SignupRequest;
+import at.aau.recipeorganizer.data.User;
 import at.aau.recipeorganizer.repository.RecipeRepository;
 import at.aau.recipeorganizer.repository.RoleRepository;
 import at.aau.recipeorganizer.repository.UserRepository;
@@ -9,21 +12,26 @@ import at.aau.recipeorganizer.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static at.aau.recipeorganizer.configuration.jwt.JwtUtils.JWT_COOKIE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -53,12 +61,36 @@ public class AuthControllerTest {
     @MockBean
     private JwtUtils jwtUtils;
 
+    @MockBean
+    private Authentication authentication;
+
     private ObjectMapper objectMapper;
 
     @BeforeEach
     public void init() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testAuthenticateUser() throws Exception {
+        User user = new User("testUser", "test@email.com", "testPassword");
+        user.roles.add(new Role(Role.ERole.ROLE_USER));
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(authenticationManager.authenticate(Mockito.any(Authentication.class))).thenReturn(authentication);
+
+        ResponseCookie jwtCookie = ResponseCookie.from(JWT_COOKIE, "1234567890").path("/api").maxAge((long) 24 * 60 * 60).httpOnly(true).build();
+        when(jwtUtils.generateJwtCookie(Mockito.any(User.class))).thenReturn(jwtCookie);
+
+        LoginRequest request = new LoginRequest("testUser", "testPassword");
+        mockMvc.perform(post("/api/auth/signin")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value("testUser"));
     }
 
     @Test
