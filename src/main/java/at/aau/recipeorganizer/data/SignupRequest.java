57234acb768a768a281ee79b835eda033c05c291
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-22 07:46:18 +0100

    Sonarcloud: Remove this unused import 'java.util.Set'.

--- src/main/java/at/aau/recipeorganizer/data/SignupRequest.java
+++ src/main/java/at/aau/recipeorganizer/data/SignupRequest.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import java.util.Set;
-
 public record SignupRequest(
         @NotBlank
         @Size(min = 3, max = 20)
