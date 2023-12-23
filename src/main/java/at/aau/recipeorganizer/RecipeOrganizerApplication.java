8e6725454c1468e5374950b762bf84d967664d11
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-23 10:53:24 +0100

    Readded cors configuration which was lost during merge

--- src/main/java/at/aau/recipeorganizer/RecipeOrganizerApplication.java
+++ src/main/java/at/aau/recipeorganizer/RecipeOrganizerApplication.java
@@ -10,11 +10,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipeOrganizerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeOrganizerApplication.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+        };
     }
 
     @Bean
