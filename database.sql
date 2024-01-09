68d5a49f9f4c0bf557359c3cb464fb43847c9c5a
Stefan Schellander <stefansche@edu.aau.at>
2024-01-09 18:05:19 +0100

    test database connection

--- database.sql
+++ database.sql
@@ -41,3 +41,4 @@ INSERT INTO user_roles (user_id, role_id)
 VALUES ('2', (SELECT role_id FROM roles WHERE name = 'ROLE_USER'));
 
 \connect postgresql://postgres:postgres@postgres:5432/recipeorganizer
+\connect postgres://postgres:postgres@postgres:5432/recipeorganizer
