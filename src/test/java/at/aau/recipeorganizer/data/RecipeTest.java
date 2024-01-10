f4566f372fb89d935136d0c62d885c200b40c432
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2024-01-10 17:18:13 +0100

    Added recipe tests for ingredients

--- src/test/java/at/aau/recipeorganizer/data/RecipeTest.java
+++ src/test/java/at/aau/recipeorganizer/data/RecipeTest.java
@@ -14,6 +14,16 @@ class RecipeTest {
     }
 
     @Test
+    void testEquals_NotEquals() {
+        Recipe recipe1 = new Recipe("Test Recipe", "Test Ingredient", "Test Description");
+        Recipe recipe2 = new Recipe("Test Recipe", "Test Ingredient", "Test Description");
+        recipe1.id = 1;
+        recipe2.id = 2;
+
+        assertNotEquals(recipe1, recipe2);
+    }
+
+    @Test
     void testEquals_NotEquals_WrongName() {
         Recipe recipe1 = new Recipe("Test Recipe 1", "Test Ingredient", "Test Description");
         Recipe recipe2 = new Recipe("Test Recipe 2", "Test Ingredient", "Test Description");
@@ -70,6 +80,14 @@ class RecipeTest {
     }
 
     @Test
+    void testHashCode_IngredientsNull() {
+        Recipe recipe1 = new Recipe("Test Recipe", "Test Ingredient", "Test Description 1");
+        Recipe recipe2 = new Recipe("Test Recipe", null, "Test Description 2");
+
+        assertNotEquals(recipe1.hashCode(), recipe2.hashCode());
+    }
+
+    @Test
     void testHashCode_DescriptionNull() {
         Recipe recipe1 = new Recipe("Test Recipe", "Test Ingredient", "Test Description 1");
         Recipe recipe2 = new Recipe("Test Recipe", "Test Ingredient", null);
@@ -92,6 +110,15 @@ class RecipeTest {
         recipe1.update(recipe2);
 
         assertNotEquals(recipe1.name, recipe2.name);
+    }
+
+    @Test
+    void testUpdate_IngredientsNull() {
+        Recipe recipe1 = new Recipe("Test Recipe", "Test Ingredient", "Test Description");
+        Recipe recipe2 = new Recipe("Test Recipe", null, "Test Description");
+        recipe1.update(recipe2);
+
+        assertNotEquals(recipe1.ingredients, recipe2.ingredients);
     }
 
     @Test
