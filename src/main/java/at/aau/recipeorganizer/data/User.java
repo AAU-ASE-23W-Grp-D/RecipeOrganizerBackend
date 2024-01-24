12b1c3c0b22e5bc7bb1055abe87b6f2633f20d23
Stefan Schellander <stefansche@edu.aau.at>
2024-01-24 14:04:39 +0100

    fixed/removed/ "High" code smell - assertion - serializable

--- src/main/java/at/aau/recipeorganizer/data/User.java
+++ src/main/java/at/aau/recipeorganizer/data/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,7 +21,7 @@ import java.util.stream.Collectors;
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
         })
-public class User implements UserDetails {
+public class User implements UserDetails, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
